34570e167b835820d0aa0c01162bbedab0e029cb
xobaib <xobaib2001.m@gmail.com>
2025-01-28 15:24:19 +0330

    Fetching User Input

--- src/app.ts
+++ src/app.ts
@@ -37,15 +37,38 @@ class ProjectInput {
     this.attach();
   }
 
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement?.value;
+    const enteredDescription = this.descriptionInputElement?.value;
+    const enteredPeople = this.peopleInputElement?.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert("Invalid input, please try again!");
+      return;
+    }
+    return [enteredTitle, enteredDescription, Number(enteredPeople)];
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
+  }
+
   @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
+    const userInput = this.gatherUserInput();
 
-    if (this.titleInputElement?.value.trim().length <= 0) {
-      alert("Title input should not be empty");
-      return;
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+      this.clearInputs();
     }
-    console.log(this.titleInputElement?.value.trim());
   }
 
   private configure() {
