7c3eabad7f93a150016e45c8d61de9b50d67ac97
xobaib <xobaib2001.m@gmail.com>
2025-01-28 11:25:59 +0330

    DOM Element Selection OOP Rendering

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,43 @@
+// const projectInput = document.getElementById(
+//   "project-input"
+// ) as HTMLTemplateElement;
+// const app = document.getElementById("app");
+// const singleProject = document.getElementById(
+//   "single-project"
+// ) as HTMLTemplateElement;
+// const projectList = document.getElementById(
+//   "project-list"
+// ) as HTMLTemplateElement;
 
+// app?.append(
+//   projectInput?.content.cloneNode(true),
+//   singleProject?.content.cloneNode(true),
+//   projectList?.content.cloneNode(true)
+// );
+
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    ) as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app") as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const projectInput = new ProjectInput();
