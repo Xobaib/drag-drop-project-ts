491ad7a0c5775b7845d13a21902b5c8828af724a
xobaib <xobaib2001.m@gmail.com>
2025-01-28 12:27:34 +0330

    Interacting with DOM Elements

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,40 @@
-// const projectInput = document.getElementById(
-//   "project-input"
-// ) as HTMLTemplateElement;
-// const app = document.getElementById("app");
-// const singleProject = document.getElementById(
-//   "single-project"
-// ) as HTMLTemplateElement;
-// const projectList = document.getElementById(
-//   "project-list"
-// ) as HTMLTemplateElement;
-
-// app?.append(
-//   projectInput?.content.cloneNode(true),
-//   singleProject?.content.cloneNode(true),
-//   projectList?.content.cloneNode(true)
-// );
-
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
-    this.templateElement = document.getElementById(
-      "project-input"
-    ) as HTMLTemplateElement;
+    this.templateElement = document.getElementById("project-input") as HTMLTemplateElement;
     this.hostElement = document.getElementById("app") as HTMLDivElement;
 
-    const importedNode = document.importNode(
-      this.templateElement.content,
-      true
-    );
+    const importedNode = document.importNode(this.templateElement.content, true);
 
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.setAttribute("id", "user-input");
+
+    this.titleInputElement = this.element.querySelector("#title") as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector("#description") as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector("#people") as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+
+    if (this.titleInputElement?.value.trim().length <= 0) {
+      alert("Title input should not be empty");
+      return;
+    }
+    console.log(this.titleInputElement?.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
   }
 
   private attach() {
