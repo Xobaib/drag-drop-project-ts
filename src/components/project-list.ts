3898e4475e34200bd49b940209eb33d697225abf
xobaib <xobaib2001.m@gmail.com>
2025-02-04 14:25:32 +0330

    Understanding various Import Export Syntaxes

--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,7 +1,6 @@
-import { Project } from "../models/project.js";
-import { Component } from "./base-component.js";
+import { Project, ProjectStatus } from "../models/project.js";
+import Component from "./base-component.js";
 import { projectState } from "../state/project-state.js";
-import { ProjectStatus } from "../models/project.js";
 import { autobind } from "../decorators/autobind.js";
 import { ProjectItem } from "./project-item.js";
 import { DragTarget } from "../models/drag-drop.js";
