e826023dddc30d227dcd19eb1fa8b7c72371757d
xobaib <xobaib2001.m@gmail.com>
2025-02-05 11:22:27 +0330

    Adding Entry Output Configuration

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,7 +1,7 @@
-import Component from "./base-component.js";
-import * as Validation from "../util/validation.js";
-import { projectState } from "../state/project-state.js";
-import { autobind as Autobind } from "../decorators/autobind.js";
+import Component from "./base-component";
+import * as Validation from "../util/validation";
+import { projectState } from "../state/project-state";
+import { autobind as Autobind } from "../decorators/autobind";
 
 // ProjectInput class
 export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,9 +1,9 @@
-import { Project, ProjectStatus } from "../models/project.js";
-import Component from "./base-component.js";
-import { projectState } from "../state/project-state.js";
-import { autobind } from "../decorators/autobind.js";
-import { ProjectItem } from "./project-item.js";
-import { DragTarget } from "../models/drag-drop.js";
+import { Project, ProjectStatus } from "../models/project";
+import Component from "./base-component";
+import { projectState } from "../state/project-state";
+import { autobind } from "../decorators/autobind";
+import { ProjectItem } from "./project-item";
+import { DragTarget } from "../models/drag-drop";
 
 // ProjectList class
 export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
